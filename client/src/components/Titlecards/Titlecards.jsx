cb1a9ef9a43772144dc371bdc52df9cf40b25b30
ketan <mail.ketan027@gmail.com>
2025-03-31 13:54:41 +0530

    Implemented Basic Navbar in react vite

---
+++ client/src/components/Titlecards/Titlecards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Titlecards.css"
+
+const Titlecards = () => {
+  return (
+    <div className='titlecards'> Titlecards</div>
+  )
+}
+
+export default Titlecards
