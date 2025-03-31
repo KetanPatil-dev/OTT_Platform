422ee66d7abef00c43dfb2c65a0dab38c3240a45
ketan <mail.ketan027@gmail.com>
2025-03-31 18:53:10 +0530

    Implemented Navbar plus basic homepage

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Navbar.css"
-import logo from "../../assets/logo.png"
+import logo from "../../assets/cloneflix.png"
 import search_icon from "../../assets/search_icon.svg"
 import bell_icon from "../../assets/bell_icon.svg"
 import profile_img from "../../assets/profile_img.png"
@@ -21,11 +21,14 @@ const Navbar = () => {
         </div>
         <div className="navbar-right"></div>
         <img src={search_icon} alt='search_icon' className='icons' />
-        <p>Children</p>
+        <p className='para'>Children</p>
         <img src={bell_icon} alt='search_icon' className='icons' />
        <div className='navbar-profile'>
         <img src={profile_img} alt='search_icon' className='profile ' />
         <img src={caret_icon} alt='search_icon' />
+        <div className="dropdown">
+            <p>Sign out of CloneFlix</p>
+        </div>
          
        </div>
     </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
-
+import hero_banner from "../../assets/hero_banner.jpg"
+import hero_title from "../../assets/hero_title.png"
 const Home = () => {
   return (
     <div className="home">
       <Navbar/>
+      <div className="hero">
+        <img alt="banner-img" className="banner-img" src={hero_banner}/>
+        <div className="hero-caption">
+            <img src={hero_title}  alt="caption-img" className="caption-img"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, voluptate reiciendis voluptas quaerat quisquam perferendis fugit a id voluptatum similique omnis temporibus sequi maxime enim nostrum soluta? Quam, autem deserunt.</p>
+        </div>
+      </div>
     </div>
   );
 };
