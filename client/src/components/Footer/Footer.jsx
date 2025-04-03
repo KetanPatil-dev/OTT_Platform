8f50543206277c457f68aaa7093ca63a335d5422
ketan <mail.ketan027@gmail.com>
2025-04-03 10:52:38 +0530

    Implemented Complete Homepage with minor UI fixes

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import "./Footer.css"
+import insta_icon from "../../assets/instagram_icon.png"
+import twitter_icon from "../../assets/twitter_icon.png"
+import youtube_icon from "../../assets/youtube_icon.png"
+import fb_icon from "../../assets/facebook_icon.png"
 
 const Footer = () => {
   return (
-    <div className='footer'> Footer</div>
+    <div className='footer'> 
+    <div className='footer-icons'>
+      <img src={insta_icon} alt="insta_icon"/>
+      <img src={twitter_icon} alt="x_icon"/>
+      <img src={youtube_icon} alt="yt_icon"/>
+      <img src={fb_icon} alt="fb_icon"/>
+    </div>
+    <ul>
+      <li>Audio Descriptoon</li>
+      <li>Help Centre</li>
+      <li>Gift Cards</li>
+      <li>Media Centre</li>
+      <li>Invester Relations</li>
+      <li>Jobs</li>
+      <li>Terms of Use</li>
+      <li>Privacy</li>
+      <li>Legal Notices</li>
+      <li>Cookie Preferences</li>
+      <li>Corporate Information</li>
+      <li>Contact Us</li>
+    </ul>
+    <p className='copyright-text'> &copy; 2002-2102 CloneFlix, Inc</p>
+      </div>
   )
 }
 
--- client/src/components/Titlecards/Titlecards.jsx
+++ client/src/components/Titlecards/Titlecards.jsx
@@ -2,11 +2,11 @@ import React  from "react";
 import "./Titlecards.css";
 import card_data from "../../assets/cards/Cards_data";
 
-const Titlecards = () => {
+const Titlecards = ({title,category}) => {
   
   return (
     <div className="title-cards">
-      <h2>Popular on CloneFlix</h2>
+      <h2>{title?title:"Popular on CloneFlix"}</h2>
       <div className="card-list">
         {card_data.map((card, index) => {
           return (
