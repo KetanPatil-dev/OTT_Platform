e2098d3d05ad43b18b2927b864801d9a25cbbcdf
ketan <mail.ketan027@gmail.com>
2025-04-03 16:15:47 +0530

    Implemented UI of Signup and Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,20 @@
 
 import Home from './pages/Home/Home'
 
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Login from "./pages/Login/Login"
 function App() {
 
 
   return (
-    <>
-      <Home/>
-    </>
+  <BrowserRouter>
+  <Routes>
+
+  <Route path='/'  element={ <Home/>} />
+  <Route path='/login'  element={<Login/>} />
+  </Routes>
+  </BrowserRouter>
+    
   )
 }
 
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
+import logo from "../../assets/cloneflix.png"
 const Login = () => {
+  const [signup,setsignup]=useState(true)
   return (
-    <div className='login'>Login</div>
+    <>
+    { signup===true && <div className='login'>
+      <img src={logo} alt='logo' />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type='text' placeholder='Name :'/>
+          <input type='email' placeholder='Email :'/>
+          <input type='password' placeholder='Password : '/>
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type='checkbox' />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          <p>Already have account ? <span onClick={()=>setsignup(false)}>Log In </span></p>
+          
+          
+        </div>
+      </div>
+    </div>}
+    {signup===false && <div className='login'>
+      <img src={logo} alt='logo' />
+      <div className="login-form">
+        <h1>Login </h1>
+        <form>
+         
+          <input type='email' placeholder='Email :'/>
+          <input type='password' placeholder='Password : '/>
+          <button>Login</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type='checkbox' />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          <p>New to CloneFlix ? <span onClick={()=>setsignup(true)}>Sign Up </span></p>
+          
+          
+        </div>
+      </div>
+    </div>}
+    </>
   )
 }
 
